bdebaaf3bc541ca5b2166fc26761f563f363a81b
HenryLampoglio <henrylampoglio@hotmail.com>
2023-03-15 15:50:39 -0300

    finished createTask function js

--- script.js
+++ script.js
@@ -4,32 +4,6 @@ const editButton = document.querySelector(".edit-button");
 const deleteButton = document.querySelector(".del-button");
 const tblBody = document.querySelector("tbody");
 
-
-let today = new Date();
-
-const formatDate = (dateUTC) =>
-{
-    const options = {dateStyle: 'long', timeStyle: 'short'};
-    const date = new Date(dateUTC).toLocaleString('pt-br', options);
-    return date;
-}
-
-const createSelect = () =>
-{
-    let options = ['pendente','em andamento','concluido'];
-    const cell = document.querySelector("tbody tr:last-child td:nth-child(3)");
-    const selectList = document.createElement('select');
-    cell.appendChild(selectList);
-
-    for(let i = 0; i < options.length; i++)
-    {
-        let option = document.createElement("option");
-        option.value = options[i];
-        option.text = options[i];
-        selectList.appendChild(option);
-    }
-}
-
 const createCell = () => 
 {
     const row = document.createElement("tr");
@@ -43,6 +17,46 @@ const createCell = () =>
     tblBody.appendChild(row);
 }
 
+let today = new Date();
+
+const formatDate = (dateUTC) =>
+{
+    const options = {dateStyle: 'long', timeStyle: 'short'};
+    const date = new Date(dateUTC).toLocaleString('pt-br', options);
+    return date;
+}
+
+
+const createSelect = () =>
+{
+    const options = ['pendente','em andamento','concluido'];
+    const cell = document.querySelector("tbody tr:last-child td:nth-child(3)");
+    const selectList = document.createElement('select');
+    cell.appendChild(selectList);
+    
+    for(let i = 0; i < options.length; i++)
+    {
+        let option = document.createElement("option");
+        option.value = options[i];
+        option.text = options[i];
+        selectList.appendChild(option);
+    }
+}
+
+const createButton = () =>
+{
+    const cell = document.querySelector("tbody tr:last-child td:nth-child(4)")
+    const editButton = document.createElement("button");
+    editButton.classList.add("edit-button");
+    editButton.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" height="48" viewBox="0 96 960 960" width="48"><path d="M480 936v-71l216-216 71 71-216 216h-71ZM120 726v-60h300v60H120Zm690-49-71-71 29-29q8-8 21-8t21 8l29 29q8 8 8 21t-8 21l-29 29ZM120 561v-60h470v60H120Zm0-165v-60h470v60H120Z"/></svg>';
+    cell.appendChild(editButton)
+
+    const deleteButton = document.createElement("button");
+    deleteButton.classList.add("del-button");
+    deleteButton.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" height="48" viewBox="0 96 960 960" width="48"><path d="M261 936q-24.75 0-42.375-17.625T201 876V306h-41v-60h188v-30h264v30h188v60h-41v570q0 24-18 42t-42 18H261Zm438-630H261v570h438V306ZM367 790h60V391h-60v399Zm166 0h60V391h-60v399ZM261 306v570-570Z"/></svg>';
+    cell.appendChild(deleteButton)
+}
+
 function addContent() {
 
     let firstCell = document.querySelector("tbody tr:last-child td:nth-child(1)");
@@ -52,9 +66,9 @@ function addContent() {
 
 
     firstCell.innerHTML = input.value;
-    secondCell.innerHTML = `${formatDate(today)}`;
+    secondCell.innerHTML = formatDate(today);
     thirdCell = createSelect();
-    fourthCell.innerHTML = `//`;
+    fourthCell = createButton();
 }
 
 const createTask = () =>
@@ -64,7 +78,7 @@ const createTask = () =>
 }
 
 
-addEventListener("submit", (form,) =>
+addEventListener("submit", (form) =>
 {
     form.preventDefault();
     createTask();
